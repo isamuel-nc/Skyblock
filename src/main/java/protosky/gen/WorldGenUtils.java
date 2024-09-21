b8d5af10bd46ae84c2da124570c9a58dce911e73
isamuel-nc <83732023+isamuel-nc@users.noreply.github.com>
2024-09-21 18:29:28 +0200

    Update WorldGenUtils.java

--- src/main/java/protosky/gen/WorldGenUtils.java
+++ src/main/java/protosky/gen/WorldGenUtils.java
@@ -1,66 +1,86 @@
-package protosky.gen;
-
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.util.collection.PackedIntegerArray;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.world.*;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.chunk.*;
+import net.minecraft.util.math.Box;
+import net.minecraft.world.chunk.ProtoChunk;
+import net.minecraft.world.gen.structure.Structure;
+import net.minecraft.world.gen.structure.StructureStart;
 
-import java.util.*;
+import java.util.Optional;
+import java.util.Set;
 
-public class WorldGenUtils
-{
-    public static boolean deleteBlocks(ProtoChunk chunk, boolean check)
-    {
+public class WorldGenUtils {
 
-        // remove blocks
+    // Define a set of blocks that should not be deleted
+    private static final Set<Block> BLOCKS_TO_KEEP = Set.of(
+        Blocks.BARREL,
+        Blocks.CHEST,
+        Blocks.OBSIDIAN
+    );
+
+    // Define a set of structures that should not be deleted
+    private static final Set<Structure> STRUCTURES_TO_KEEP = Set.of(
+        Structure.STRONGHOLD,   // Needed to find the End Portal and access The End
+        Structure.END_CITY,     // Needed for acquiring Elytra and Shulker Boxes after defeating the Ender Dragon
+        Structure.NETHER_FORTRESS, // Required to get Blaze Rods for brewing and Eyes of Ender
+        Structure.BASTION_REMNANT // Useful for getting resources like gold, Pigstep music disc, and Piglin barter opportunities
+    );
+
+    // Method to check if a block is within a whitelisted structure
+    public static boolean isBlockInWhitelistedStructure(WorldAccess world, BlockPos pos) {
+        StructureManager structureManager = world.getStructureManager();
+        Chunk chunk = world.getChunk(pos);
+        
+        // Iterate through all whitelisted structures
+        for (Structure structure : STRUCTURES_TO_KEEP) {
+            Optional<? extends StructureStart> structureStartOptional = chunk.getStructureStart(structure);
+            if (structureStartOptional.isPresent()) {
+                StructureStart structureStart = structureStartOptional.get();
+                Box boundingBox = structureStart.getBoundingBox();
+                
+                // Check if the block is within the structure's bounding box
+                if (boundingBox.contains(pos.getX(), pos.getY(), pos.getZ())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Method to check if a block is in the whitelist of blocks to keep
+    public static boolean isBlockInWhitelist(BlockState blockState) {
+        return BLOCKS_TO_KEEP.contains(blockState.getBlock());
+    }
+
+    public static boolean deleteBlocks(ProtoChunk chunk, boolean check, WorldAccess world) {
         boolean reset = false;
         ChunkSection[] sections = chunk.getSectionArray();
+
         for (int i = 0; i < sections.length; i++) {
             ChunkSection chunkSection = sections[i];
-            if(check || !chunkSection.isEmpty()) {
+            if (check || !chunkSection.isEmpty()) {
                 reset = true;
                 PalettedContainer<BlockState> blockStateContainer = new PalettedContainer<>(Block.STATE_IDS, Blocks.AIR.getDefaultState(), PalettedContainer.PaletteProvider.BLOCK_STATE);
                 ReadableContainer<RegistryEntry<Biome>> biomeContainer = chunkSection.getBiomeContainer();
+
+                for (BlockPos pos : BlockPos.iterate(0, 0, 0, 15, chunk.getHeight() - 1, 15)) {
+                    BlockState state = chunkSection.getBlockState(pos.getX(), pos.getY(), pos.getZ());
+
+                    boolean isInStructureWhitelist = isBlockInWhitelistedStructure(world, pos);
+                    boolean isInBlockWhitelist = isBlockInWhitelist(state);
+                    if (isInStructureWhitelist || isInBlockWhitelist) {
+                        blockStateContainer.set(pos.getX(), pos.getY(), pos.getZ(), state);  // Keep block
+                    } else {
+                        blockStateContainer.set(pos.getX(), pos.getY(), pos.getZ(), Blocks.AIR.getDefaultState());  // Delete block
+                    }
+                }
+                
                 sections[i] = new ChunkSection(blockStateContainer, biomeContainer);
             }
         }
 
-        // remove block entities
-        for (BlockPos bePos : chunk.getBlockEntityPositions())
-        {
+        for (BlockPos bePos : chunk.getBlockEntityPositions()) {
             chunk.removeBlockEntity(bePos);
         }
 
         return reset;
-    }
-
-    public static void genHeightMaps(ProtoChunk chunk)
-    {
-
-        // set heightmaps
-        int elementBits = MathHelper.ceilLog2(chunk.getHeight() + 1);
-        long[] emptyHeightmap = new PackedIntegerArray(elementBits, 256).getData();
-        for (Map.Entry<Heightmap.Type, Heightmap> heightmapEntry : chunk.getHeightmaps())
-        {
-            heightmapEntry.getValue().setTo(chunk, heightmapEntry.getKey(), emptyHeightmap);
-        }
-
-    }
-
-    public static boolean insideBorder(ChunkPos pos)
-    {
-        return pos.x < 1874999 && pos.x > -1875000 && pos.z < 1874999 && pos.z > -1875000;
-    }
-
-    public static boolean insideBorder(BlockPos pos)
-    {
-        return pos.getX() < 29999968 && pos.getX() > -29999969 && pos.getZ() < 29999968 && pos.getZ() > -29999969;
     }
 }
